07a1fd5dc296a18630e7242f4b646e0e892c7a49
willypelz <pelumiasefon@gmail.com>
2022-03-25 12:15:36 +0100

    [IMP] add database seeder

--- task-two/database/seeders/ArticlesTableDataSeeder.php
+++ task-two/database/seeders/ArticlesTableDataSeeder.php
@@ -3,8 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Article;
 
-class DatabaseSeeder extends Seeder
+class ArticlesTableDataSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call('ArticlesTableDataSeeder');
+        //
+        Article::factory()->count(5)->create();
     }
 }
